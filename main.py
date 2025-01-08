005fcc6637912c711dd9b3dc5797fd22de9412a9
vonedward <me@edward-vonschondorf.dev>
2025-01-07 22:37:09 -0800

    Add sprite groups

--- main.py
+++ main.py
@@ -6,18 +6,27 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updateable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-    dt = 0
     
+    dt = 0
     
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            
-        player.update(dt)    
+        
+        for obj in updateable:
+            obj.update(dt)
+
         screen.fill("black")
-        player.draw(screen)
+
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
         # use deltatime to limit framerate to 60 fps
         dt = clock.tick(60) / 1000
