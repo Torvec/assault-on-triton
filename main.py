9cf20be8a93a74af2747ede31516d60d76d68ca8
vonedward <me@edward-vonschondorf.dev>
2025-08-13 14:37:23 -0700

    Add player sprite with basic collision and movement

--- main.py
+++ main.py
@@ -1,23 +1,30 @@
 import pygame
 from global_consts import *
+from player import Player
 
 def main():
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    running = True
     dt = 0
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updateable, drawable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    running = True
 
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
         
+        updateable.update(dt)
+
         screen.fill("black")
 
+        for obj in drawable:
+            obj.draw(screen)
+            
         pygame.display.flip()
 
         dt = clock.tick(60) / 1000
